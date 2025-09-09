3a42f53cc22ee9bfc610e9b6152f24914572b363
Patrick Siqueira <patrickgsiqueira@hotmail.com>
2025-09-08 23:05:03 -0300

    testando

--- src/app.js
+++ src/app.js
@@ -84,7 +84,7 @@ const PORT = process.env.PORT || 3001;
 const startServer = async () => {
   try {
     // Sincroniza o banco de dados forçando recriação das tabelas
-    await db.sequelize.sync({ force: true }); 
+    await db.sequelize.sync({ force: false }); 
     console.log('Banco de dados sincronizado com sucesso (force: true).');
 
     // Inicia uma única transação para todo o processo de seeding
